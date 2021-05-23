e88f1504d7213572029b74d42e7458a1ee2e2aef
TDLorenz <troydaniello.nyc@gmail.com>
2021-05-22 20:36:06 -0400

    updated app summary on homepage

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -3,7 +3,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import styles from '../'
+import styles from '../styles/index.css'
 
 import { Link } from 'react-router-dom';
 
@@ -26,7 +26,7 @@ const useStyles = makeStyles(theme => ({
   greeting: {
     display: 'flex',
     justifyContent: 'center',
-    backgroundColor: #4E5D94,
+    backgroundColor: white,
     width: "50%",
     margin: "auto",
   },
@@ -60,9 +60,18 @@ const HomePageView = () => {
         </Toolbar>
       </AppBar>
 
-      <div className={classes.greeting}><h1>Campus Student Database CRUD App</h1></div>
-      <section className="layout">
-        This website introduces
+      <div className={classes.greeting}><h1>Campus and Student Interactive Management Simulation</h1></div>
+      <section className={styles.layout}>
+        <h1>Welcome To The Home Page!</h1>
+        <br />
+        <br />
+        <br />
+        <p>
+          This RESTful full-stack web app simualtes the management of some students and campuses. Students and Campuses
+          can be viewed via navigating the differnt links above on the navbar and are seperated. It is possible to view
+          single campuses and students as well as all of them. Student and Campus entries can be deleted or updated by adding new entries.
+          The app will track these and update the entries via a database.
+        </p>
       </section>
     </div>
   );
