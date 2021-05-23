f4020407d887f30404c66f535c09a6e3389b90b9
TDLorenz <troydaniello.nyc@gmail.com>
2021-05-22 20:29:33 -0400

    added title and basic paragraph to home page

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -3,6 +3,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import styles from '../'
 
 import { Link } from 'react-router-dom';
 
@@ -14,22 +15,22 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     textAlign: 'left',
     fontType: 'bold',
-    fontFamily: 'Courier, sans-serif', 
-    fontSize: '35px', 
+    fontFamily: 'Courier, sans-serif',
+    fontSize: '35px',
     color: '#CDDC39'
   },
-  appBar:{
+  appBar: {
     backgroundColor: '#11153e',
     shadows: ['none'],
   },
-  greeting:{
+  greeting: {
     display: 'flex',
     justifyContent: 'center',
-    backgroundColor: 'white',
+    backgroundColor: #4E5D94,
     width: "50%",
     margin: "auto",
   },
-  links:{
+  links: {
     textDecoration: 'none',
   }
 
@@ -46,7 +47,7 @@ const HomePageView = () => {
           </Typography>
 
           <Link className={classes.links} to={'/campuses'} >
-            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+            <Button variant="contained" color="primary" style={{ marginRight: '10px' }}>
               All Campuses
             </Button>
           </Link>
@@ -58,10 +59,13 @@ const HomePageView = () => {
           </Link>
         </Toolbar>
       </AppBar>
-      
-      <div className={classes.greeting}><h1>Home Page</h1></div>
+
+      <div className={classes.greeting}><h1>Campus Student Database CRUD App</h1></div>
+      <section className="layout">
+        This website introduces
+      </section>
     </div>
-  );    
+  );
 }
 
 
