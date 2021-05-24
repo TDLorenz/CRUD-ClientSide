a7fc19db901ee4aec7e6f119c30043a1f068aa2a
TDLorenz <troydaniello.nyc@gmail.com>
2021-05-23 20:48:02 -0400

    got rid of spacing in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Footer extends Component {
     render() {
@@ -10,15 +9,12 @@ class Footer extends Component {
                     <p>
                         Vladimir Andreev &emsp; &nbsp; &nbsp; <a href="https://github.com/VladimirAndreev09">https://github.com/VladimirAndreev09</a>
                     </p>
-                    <br />
                     <p>
                         Troy Daniello &emsp; &emsp; &ensp; &ensp; <a href="https://github.com/TDLorenz">https://github.com/TDLorenz</a>
                     </p>
-                    <br />
                     <p>
                         Sajarin Dider &emsp; &emsp; &ensp; &ensp; <a href="https://github.com/sajarindider">https://github.com/sajarindider</a>
                     </p>
-                    <br />
                     <p>
                         Valentine Shidlovskiy &nbsp; <a href="https://github.com/Valentine-S">https://github.com/Valentine-S</a>
                     </p>
